4629157e5205cc8840044c6d1a90be52cb694ef4
Felix Kröner <felix.kroener@dynamic-biosensors.com>
2025-03-08 22:01:28 +0100

    test: Add diff-pdf verification into docker container

--- test/.run-tests.sh
+++ test/.run-tests.sh
@@ -5,9 +5,5 @@ gem build asciidoctor-mathjax.gemspec
 mv asciidoctor-mathjax-*.gem ./test/asciidoctor-mathjax-test.gem
 
 cd ./test || exit
-docker build --quiet --tag $IMAGE_TAG .
-docker run --rm -it -v "$PWD/:/test" $IMAGE_TAG ./convert-to-pdf.sh \
-  ./verification/asciimath.adoc \
-  ./verification/latex.adoc \
-  ./verification/stem_asciimath.adoc \
-  ./verification/stem_latex.adoc
+docker build --tag $IMAGE_TAG .
+docker run --rm -it -v "$PWD/:/test" $IMAGE_TAG ./verify.sh asciimath latex stem_asciimath stem_latex
---
+++ test/verify.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+function convert {
+  local file="$1"
+  asciidoctor-pdf \
+    --require asciidoctor-mathjax \
+    --attribute root="${PWD}" \
+    --failure-level=INFO \
+    --verbose \
+    --trace \
+    "./verification/$file.adoc"
+}
+
+function verify {
+  local test_case="$1"
+  local received=./verification/"$test_case".pdf
+  local verified=./verification/"$test_case".verified.pdf
+  local diff=./verification/"$test_case".diff.pdf
+
+  if xvfb-run diff-pdf --output-diff="$diff" "$received" "$verified" ; then
+      echo "PASS: Verification of $test_case.adoc"
+  else
+      >&2 echo "FAILED: Verification of $test_case.adoc"
+  fi
+}
+
+
+gem install ./asciidoctor-mathjax-test.gem
+
+for file in "$@"; do
+  convert "$file"
+  verify "$file"
+done
